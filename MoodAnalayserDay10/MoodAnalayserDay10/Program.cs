23fbcb2d17c1dcd47a470dca05dd503b2b959c15
sonal <sonalkarle01@gmail.com>
2021-02-12 17:10:12 +0530

    Welcome

---
+++ MoodAnalayserDay10/MoodAnalayserDay10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MoodAnalyserOfPerson
+{
+    public class MoodAnalyser
+    {
+        string Message;
+
+        public MoodAnalyser()
+        {
+        }
+
+        public MoodAnalyser(string Message)
+        {
+            this.Message = Message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (Message.ToLower().Contains("sad"))
+                return "SAD";
+            else return "HAPPY";
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
