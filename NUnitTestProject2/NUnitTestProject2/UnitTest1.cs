729f8e564382585e2d9afac46609c7a7d36faafe
sonal <sonalkarle01@gmail.com>
2021-02-12 18:16:51 +0530

    Pass Handle null scenario using try catch

--- NUnitTestProject2/NUnitTestProject2/UnitTest1.cs
+++ NUnitTestProject2/NUnitTestProject2/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace MoodAnalyserNUnitTest
             moodAnalyser = new MoodAnalyser();
         }
         /// <summary>
-        /// TC1.1 : Given message " I am in sad Mood " message in construator  should return SAD
+        /// TC1.1: Given message " I am in sad Mood " in contructor should return Constructor Sad
         /// </summary>
         [Test]
         public void GivenMessage_WhenSad_ShouldReturnSad()
@@ -21,9 +21,8 @@ namespace MoodAnalyserNUnitTest
             Assert.AreEqual("SAD", message);
         }
         /// <summary>
-        /// TC1.2: Given message " I am in Happy Mood " message in constructor should return Happy
+        /// TC1.2 : Given message " I am in Happy Mood " in construvtor should return Construcor Happy
         /// </summary>
-
         [Test]
         public void GivenMessage_WhenHappy_ShouldReturnHappy()
         {
@@ -31,6 +30,15 @@ namespace MoodAnalyserNUnitTest
             string message = moodAnalyser.AnalyseMood();
             Assert.AreEqual("HAPPY", message);
         }
-
+        /// <summary>
+        /// Given null mood should return Happy
+        /// </summary>
+        [Test]
+        public void GivenMessage_WhenNull_ShouldReturnHappy()
+        {
+            moodAnalyser = new MoodAnalyser();
+            string Message = moodAnalyser.AnalyseMood();
+            Assert.AreEqual("HAPPY", Message);
+        }
     }
 }
